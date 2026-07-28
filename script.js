20b7b191919c56757c5eda7a1b5ab7fa6098a5a3
Palak <161704641+Palak-0704@users.noreply.github.com>
2026-07-28 13:28:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -70,7 +70,7 @@ async function startAR() {
 startAR();
  */
 
-import * as THREE from "three";
+/* import * as THREE from "three";
 import { MindARThree } from "mind-ar/dist/mindar-image-three.prod.js";
 
 const mindarThree = new MindARThree({
@@ -104,6 +104,71 @@ async function startAR() {
     await mindarThree.start();
 
     console.log("AR STARTED");
+
+    renderer.setAnimationLoop(() => {
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
+
+        renderer.render(scene, camera);
+    });
+}
+
+startAR(); */
+import * as THREE from "three";
+import { MindARThree } from "mind-ar/dist/mindar-image-three.prod.js";
+
+const mindarThree = new MindARThree({
+    container: document.body,
+    imageTargetSrc: "/targets.mind"
+});
+
+const { renderer, scene, camera } = mindarThree;
+
+const anchor = mindarThree.addAnchor(0);
+
+const status = document.createElement("div");
+
+status.style.position = "fixed";
+status.style.top = "20px";
+status.style.left = "20px";
+status.style.zIndex = "9999";
+status.style.color = "white";
+status.style.background = "black";
+status.style.padding = "10px";
+
+status.innerText = "Searching for image...";
+
+document.body.appendChild(status);
+
+anchor.onTargetFound = () => {
+    status.innerText = "IMAGE FOUND";
+};
+
+anchor.onTargetLost = () => {
+    status.innerText = "IMAGE LOST";
+};
+
+const geometry = new THREE.BoxGeometry(
+    0.3,
+    0.3,
+    0.3
+);
+
+const material = new THREE.MeshBasicMaterial({
+    color: 0xff0000
+});
+
+const cube = new THREE.Mesh(
+    geometry,
+    material
+);
+
+cube.position.set(0, 0, 0.2);
+
+anchor.group.add(cube);
+
+async function startAR() {
+    await mindarThree.start();
 
     renderer.setAnimationLoop(() => {
         cube.rotation.x += 0.01;
