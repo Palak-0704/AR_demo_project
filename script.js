367f150e84d6215c8ba5e23bbc407d6de48ff316
Palak <161704641+Palak-0704@users.noreply.github.com>
2026-07-28 12:55:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-import * as THREE from  "three";
+import * as THREE from "three";
 import { MindARThree } from "mind-ar/dist/mindar-image-three.prod.js";
+
 const mindarThree = new MindARThree({
-    container:document.body,
-    imageTargetSrc:"/targets.mind"
+    container: document.body,
+    imageTargetSrc: "/targets.mind"
 });
 
+const { renderer, scene, camera } = mindarThree;
+
+// Target image ka anchor
+const anchor = mindarThree.addAnchor(0);
+
+
+// Status message
 const status = document.createElement("div");
 
 status.style.position = "fixed";
@@ -19,56 +27,49 @@ status.innerText = "Searching for image...";
 
 document.body.appendChild(status);
 
+
+// Image detect hone par
 anchor.onTargetFound = () => {
-    status.innerText = "IMAGE FOUND ";
+    status.innerText = "IMAGE FOUND";
 };
 
+
+// Image detect na hone par
 anchor.onTargetLost = () => {
-    status.innerText = "IMAGE LOST ";
+    status.innerText = "IMAGE LOST";
 };
-/* const scene = new THREE.Scene(); 
-const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth/window.innerHeight,
-    0.1,
-    1000
-);
-camera.position.z=5;
-const render = new THREE.WebGLRenderer();
-render.setSize(
-    window.innerWidth,
-    window.innerHeight
-);
-document.body.appendChild(
-    render.domElement
-); */
-const geometry= new THREE.BoxGeometry(
+
+
+// Cube
+const geometry = new THREE.BoxGeometry(
     0.2,
     0.2,
     0.2
 );
 
-const material=  new THREE.MeshBasicMaterial({
-    color:0xff0000
+const material = new THREE.MeshBasicMaterial({
+    color: 0xff0000
 });
-const cube= new THREE.Mesh(
+
+const cube = new THREE.Mesh(
     geometry,
     material
 );
+
+
+// Cube ko target image ke anchor ke saath attach karo
 anchor.group.add(cube);
 
-/* scene.add(cube);
-cube.rotation.x = 0.5;
-cube.rotation.y = 0.5;
-render.render(
-    scene,
-    camera
-); */
 
+// AR start
 async function startAR() {
+
     await mindarThree.start();
-    renderer.setAnimationLoop(()=>{
-        renderer.render(scene,camera);
+
+    renderer.setAnimationLoop(() => {
+        renderer.render(scene, camera);
     });
+
 }
+
 startAR();
